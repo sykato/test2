14cd2374ece381a005b52413cec0bcfb961700c2
Tereschenko Aleksandr <tere@WIN-JUB8CCCU1GA.DOMAIN.local>
2022-02-12 10:45:55 +0300

    programm

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class programm
+{
+    static void Main()
+    {
+        int year;
+        Console.Write("Введите год: ");
+        year = Convert.ToInt32(Console.ReadLine());
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+            Console.WriteLine("год является високосным");
+        else
+            Console.WriteLine("год неявляется високосным");
+    }
+}
